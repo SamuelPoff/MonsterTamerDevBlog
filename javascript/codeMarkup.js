6ec9394fbdcf764ed245b10f16d5f9017530d54f
Samuel Poff <samuelpoff21@gmail.com>
2024-08-13 18:10:25 -0700

    codeMarkup.js added. Automatic code markup system started.

---
+++ javascript/codeMarkup.js
@@ -0,0 +1,35 @@
+
+let codeSamples = document.getElementsByClassName("codeSample");
+
+const KEYWORDS_LIST = ["new", "public", "private", "int", "bool", "string", "float", "class", "var", "get", "set", "return"];
+const WHITESPACE_LIST = [' ', '\t', '\n', '\r'];
+
+console.log("Start");
+
+for(let i = 0; i < codeSamples.length; ++i){
+
+    let codeSample = codeSamples[i];
+    let codeText = codeSample.textContent;
+    
+    let spaceIndex = codeText.indexOf(" ");
+    codeText.split
+
+
+}
+
+function getKeywordsIndices(codeText){
+
+    
+    for(let i =0;i < symbols.length; ++i){
+
+
+
+    }
+
+}
+
+function getNextWord(text){
+
+    let string = "*\g/g[ae]y/*";
+
+}
